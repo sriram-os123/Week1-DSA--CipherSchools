b0606cce41d487e82e4f31d1dbd1c60446fce927
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-06-19 17:44:38 +0530

    Update and rename Lecture3 to Lecture3.cpp

---
+++ Lecture3.cpp
@@ -0,0 +1,10 @@
+//i have learnt about datatypes from this lecture and utilizing in the program
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	int b;
+	cin>>a>>b;
+	cout<<a<<"\n"<<b;
+	return 0;
+}
