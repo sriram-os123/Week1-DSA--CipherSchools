f57e4dc813adcad670de98b4fc0196eb18d7d16a
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-06-21 15:31:13 +0530

    Create Lecture9.cpp

---
+++ Lecture9.cpp
@@ -0,0 +1,16 @@
+// from this lecture i have learnt about for loops introduction 
+#include <iostream>
+using namespace std;
+int main() {
+    int a[5];
+    int i;
+    cout<<"Give 5 integers as input"<<endl;
+    for(i=0;i<5;i++){
+        cin>>a[i];
+    }
+    for(i=5;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+
+    return 0;
+}
